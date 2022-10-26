ff7b4f977fac74d82487e0c053e1b2a47fb7c3ea
anandgitacc <anand.akmail@gmail.com>
2022-10-26 12:03:17 +0530

    sivagit

--- src/org/gittest1/EmployeeDetails.java
+++ src/org/gittest1/EmployeeDetails.java
@@ -10,6 +10,10 @@ public class EmployeeDetails {
 		System.out.println("8939278836");
 	}
 	
+	private void empAdd() {
+		System.out.println("Chennai-OMR");
+	}
+	
 	public static void main(String[] args) {
 		
 		EmployeeDetails e = new EmployeeDetails();
